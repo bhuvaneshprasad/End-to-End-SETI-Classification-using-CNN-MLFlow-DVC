207e42ace8ecf94a90bed093db7bc17f1be35361
Bhuvanesh Prasad <pbhuvanesh3@gmail.com>
2024-07-03 20:11:15 +0530

    replace sequential api with functional api

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
 from cnnClassifier import logger
 
-STAGE_NAME = "Data Ingestion Stage"
+# STAGE_NAME = "Data Ingestion Stage"
 
-try:
-    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-    obj = DataIngestionPipeline()
-    obj.main()
-    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
-except Exception as e:
-    logger.exception(e)
-    raise e
+# try:
+#     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+#     obj = DataIngestionPipeline()
+#     obj.main()
+#     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
 
 STAGE_NAME = "Prepare Base Model"
 
