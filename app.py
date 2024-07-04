7a7f8857cfd803bf3494dd54ee2f5f347e30a34f
Bhuvanesh Prasad <pbhuvanesh3@gmail.com>
2024-07-04 12:11:18 +0530

    updated FastAPI

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-import json
 import os
 import shutil
 from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from cnnClassifier.pipeline.prediction import PredictionPipeline
 
 app = FastAPI()
 
-@app.post("/predict/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/predict")
 async def classification(file: UploadFile):
     temp_file_path = f"temp_{file.filename}"
     
@@ -20,7 +28,7 @@ async def classification(file: UploadFile):
     finally:
         os.remove(temp_file_path)
 
-    return json.dumps([{ "prediction" : prediction}])
+    return {'prediction' : prediction}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=7384)
